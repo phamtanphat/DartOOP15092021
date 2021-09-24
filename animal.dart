2c68ed47d801db09cb9b09b7b70720ab74411ed6
phamtanphat <phatdroid94@gmail.com>
2021-09-24 19:58:16 +0700

    Initial commit

---
+++ animal.dart
@@ -0,0 +1,6 @@
+class Animal{
+  // 1 : thuoc tinh
+  late String name;
+  late int weight;
+
+}
---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'animal.dart';
+
+void main(){
+
+  // class , object
+
+  var cat = Animal();
+  cat.name = "Mèo";
+  cat.weight = 2;
+
+  print("${cat.name} ${cat.weight} kg");
+}
